4fdd2bb1417e1f84c21efc4218be338d4097e4ca
shashank agrawal <agrawalshashank51@gmail.com>
2024-06-28 03:03:27 +0530

    hu

--- src/helper/getDataFromToken.js
+++ src/helper/getDataFromToken.js
@@ -24,16 +24,53 @@
 
 // }
 
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
+// import { cookies } from 'next/headers';
+
+// export async function GetDataFromToken() {
+//     try {
+//         const allCookies = cookies().getAll();
+//         const token = allCookies.find(cookie => cookie.name === "token")?.value || '';
+//         const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+//         return decodedToken.id ;
+//     } catch (error) {
+//         throw new Error(error.message);
+//     }
+// }
+
+// import { cookies } from 'next/headers'
+ 
+// export async function getCookieData() {
+//   const cookieData = cookies().getAll()
+//   const token = cookieData.find(cookie => cookie.name === "token")?.value || '';
+//   const resolve(token){
+//     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+//     return decodedToken.id ;
+//   }
+  
+//   return new Promise((resolve) =>
+//     setTimeout(() => {
+//       resolve(token)
+//     }, 1000)
+//   )
+// }\
+
+import jwt from 'jsonwebtoken';
 import { cookies } from 'next/headers';
 
-export async function GetDataFromToken() {
-    try {
-        const allCookies = cookies().getAll();
-        const token = allCookies.find(cookie => cookie.name === "token")?.value || '';
+export async function getCookieData() {
+  const cookieData = cookies().getAll();
+  const token = cookieData.find(cookie => cookie.name === "token")?.value || '';
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
         const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-        return decodedToken.id ;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+        resolve(decodedToken.id);
+      } catch (error) {
+        reject(new Error(error.message));
+      }
+    }, 1000);
+  });
 }
+
