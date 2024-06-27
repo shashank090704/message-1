db2c317928b982f2fc3a4ccbdef1bdd7d3e8bb63
shashank agrawal <agrawalshashank51@gmail.com>
2024-06-28 02:29:30 +0530

    hi

--- src/helper/getDataFromToken.js
+++ src/helper/getDataFromToken.js
@@ -11,12 +11,11 @@
 //     }
 
 // }
-import { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
 export const GetDataFromToken = (request) => {
     try {
-        const token = request.cookies.get("token")?.value || '';
+        const token = request.cookies.get("token").value || '';
         const decodedToken = jwt.verify(token, process.env.TOOKEN_SECREAT);
         return decodedToken.id;
     } catch (error) {
